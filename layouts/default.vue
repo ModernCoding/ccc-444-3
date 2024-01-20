64a5b9fbedf40904fadd0e023e999a5675109b40
ccc.444 <contact@coding-crafting-consulting.com>
2024-01-20 20:12:39 +0700

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+
+<template>
+
+  <aside>
+    <div>TOTO</div>
+  </aside>
+
+  <menu>
+    <div>TITI</div>
+  </menu>
+
+
+  <main role="main">
+    <slot />
+  </main>
+  
+</template>
+
+
+<style
+  src="@/assets/sass/layouts/default.sass"
+  lang="sass"
+></style>
