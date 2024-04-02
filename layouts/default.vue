049948044ae1e3679ec1a9daa7c6acd8b2dfc8bf
ccc.444 <contact@coding-crafting-consulting.com>
2024-04-02 12:19:34 +0700

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,22 +4,33 @@
 
 <template>
 
-  <aside>
-    <div>CCC-444</div>
-  </aside>
-
-  <menu>
-    <ul>
-      <li>Our company</li>
-      <li>Our customers</li>
-      <li>Our contact</li>
-    </ul>
-  </menu>
+  <header>
+  </header>
 
 
-  <main role="main">
-    <slot />
-  </main>
+  <section>
+
+    <aside>
+      <div>CCC-444</div>
+    </aside>
+
+    <menu>
+      <ul>
+        <li>Our company</li>
+        <li>Our customers</li>
+        <li>Our contact</li>
+      </ul>
+    </menu>
+
+    <main role="main">
+      <slot />
+    </main>
+    
+  </section>
+
+
+  <footer>
+  </footer>
   
 </template>
 
