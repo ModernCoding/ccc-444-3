2ab6433592dd19088f0afcb4619ede80d254574e
ccc.444 <contact@coding-crafting-consulting.com>
2024-04-20 09:40:39 +0700

    getWindowWidths

---
+++ utils/grid.js
@@ -0,0 +1,149 @@
+/*
+  XS, 0
+  
+  SM_HALF, 299.434588481236
+  SM_MINUS, 343.121506177483
+  SM, 370.121506177483
+  SM_PLUS, 399.246117974981
+  SM_TWICE, 457.495341569977
+  
+  MD_HALF, 484.495341569977
+  MD_MINUS, 555.182259266225
+  MD, 598.869176962472
+  MD_PLUS, 645.99378875997
+  MD_TWICE, 740.243012354966
+
+  LG_HALF, 783.929930051213
+  LG_MINUS, 898.303765443708
+  LG, 968.990683139955
+  LG_PLUS, 1045.23990673495
+  LG_TWICE, 1197.73835392494
+
+  XL_HALF, 1268.42527162119
+  XL_MINUS, 1453.48602470993
+  XL, 1567.85986010243
+  XL_PLUS, 1691.23369549492
+  XL_TWICE, 1937.98136627991
+
+  XXL_HALF, 2052.35520167246
+  XXL_MINUS, 2351.78979015369
+  XXL, 2536.85054324244
+  XXL_PLUS, 2736.47360222994
+  XXL_TWICE, 3135.71972020492
+*/
+
+  
+const XS = 0
+
+const SM_HALF = ONCE_54_BY_PHI_POWER_MINUS_4 * PHI / 2
+const SM_MINUS = ONCE_54_BY_PHI_POWER_MINUS_4 / PHI * 3 / 2
+const SM = ONCE_54_BY_PHI_POWER_MINUS_4
+const SM_PLUS = ONCE_54_BY_PHI_POWER_MINUS_4 * PHI * 2 / 3
+const SM_TWICE = TWICE_54_BY_PHI_POWER_MINUS_3
+
+const MD_HALF = ONCE_54_BY_PHI_POWER_MINUS_5 * PHI / 2
+const MD_MINUS = ONCE_54_BY_PHI_POWER_MINUS_5 / PHI * 3 / 2
+const MD = ONCE_54_BY_PHI_POWER_MINUS_5
+const MD_PLUS = ONCE_54_BY_PHI_POWER_MINUS_5 * PHI * 2 / 3
+const MD_TWICE = TWICE_54_BY_PHI_POWER_MINUS_4
+
+const LG_HALF = ONCE_54_BY_PHI_POWER_MINUS_6 * PHI / 2
+const LG_MINUS = ONCE_54_BY_PHI_POWER_MINUS_6 / PHI * 3 / 2
+const LG = ONCE_54_BY_PHI_POWER_MINUS_6
+const LG_PLUS = ONCE_54_BY_PHI_POWER_MINUS_6 * PHI * 2 / 3
+const LG_TWICE = TWICE_54_BY_PHI_POWER_MINUS_5
+
+const XL_HALF = ONCE_54_BY_PHI_POWER_MINUS_7 * PHI / 2
+const XL_MINUS = ONCE_54_BY_PHI_POWER_MINUS_7 / PHI * 3 / 2
+const XL = ONCE_54_BY_PHI_POWER_MINUS_7
+const XL_PLUS = ONCE_54_BY_PHI_POWER_MINUS_7 * PHI * 2 / 3
+const XL_TWICE = TWICE_54_BY_PHI_POWER_MINUS_6
+
+const XXL_HALF = ONCE_54_BY_PHI_POWER_MINUS_8 * PHI / 2
+const XXL_MINUS = ONCE_54_BY_PHI_POWER_MINUS_8 / PHI * 3 / 2
+const XXL = ONCE_54_BY_PHI_POWER_MINUS_8
+const XXL_PLUS = ONCE_54_BY_PHI_POWER_MINUS_8 * PHI * 2 / 3
+const XXL_TWICE = TWICE_54_BY_PHI_POWER_MINUS_7
+
+
+const SCREEN_SIZES = [
+
+  XS,
+
+  SM_HALF,
+  SM_MINUS,
+  SM,
+  SM_PLUS,
+  SM_TWICE,
+
+  MD_HALF,
+  MD_MINUS,
+  MD,
+  MD_PLUS,
+  MD_TWICE,
+
+  LG_HALF,
+  LG_MINUS,
+  LG,
+  LG_PLUS,
+  LG_TWICE,
+
+  XL_HALF,
+  XL_MINUS,
+  XL,
+  XL_PLUS,
+  XL_TWICE,
+
+  XXL_HALF,
+  XXL_MINUS,
+  XXL,
+  XXL_PLUS,
+  XXL_TWICE
+
+]
+
+
+export const getWindowWidths = width => {
+
+  const gridIndex = (function _get (i = SCREEN_SIZES.length - 1) {
+      return i < 1 || width >= SCREEN_SIZES [ i ] ? i : _get (--i)
+    }) ()
+
+
+  return {
+
+    IsAtLeastXs: gridIndex >= 0,
+
+    IsAtLeastSmHalf: gridIndex >= 1,
+    IsAtLeastSmMinus: gridIndex >= 2,
+    IsAtLeastSm: gridIndex >= 3,
+    IsAtLeastSmPlus: gridIndex >= 4,
+    IsAtLeastSmTwice: gridIndex >= 5,
+
+    IsAtLeastMdHalf: gridIndex >= 6,
+    IsAtLeastMdMinus: gridIndex >= 7,
+    IsAtLeastMd: gridIndex >= 8,
+    IsAtLeastMdPlus: gridIndex >= 9,
+    IsAtLeastMdTwice: gridIndex >= 10,
+
+    IsAtLeastLgHalf: gridIndex >= 11,
+    IsAtLeastLgMinus: gridIndex >= 12,
+    IsAtLeastLg: gridIndex >= 13,
+    IsAtLeastLgPlus: gridIndex >= 14,
+    IsAtLeastLgTwice: gridIndex >= 15,
+
+    IsAtLeastXlHalf: gridIndex >= 16,
+    IsAtLeastXlMinus: gridIndex >= 17,
+    IsAtLeastXl: gridIndex >= 18,
+    IsAtLeastXlPlus: gridIndex >= 19,
+    IsAtLeastXlTwice: gridIndex >= 20,
+
+    IsAtLeastXxlHalf: gridIndex >= 21,
+    IsAtLeastXxlMinus: gridIndex >= 22,
+    IsAtLeastXxl: gridIndex >= 23,
+    IsAtLeastXxlPlus: gridIndex >= 24,
+    IsAtLeastXxlTwice: gridIndex >= 25
+
+  }
+
+}
