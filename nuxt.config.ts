a742d27e74162cef5a1228d5e485d50ee2b19466
ccc.444 <contact@coding-crafting-consulting.com>
2024-07-15 13:52:02 +0700

    version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig ({
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [ '~/assets/sass/app.sass' ],
-  
-  devtools: { enabled: false },
 
+  devtools: { enabled: false },
+  compatibilityDate: '2024-07-15',
 })
