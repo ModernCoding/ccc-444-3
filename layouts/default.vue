b0267a1c01e5d95990fbd5fe6560ed0a09bd25f2
ccc.444 <contact@coding-crafting-consulting.com>
2024-10-07 12:11:12 +0700

    dynamic font-size

--- layouts/default.vue
+++ layouts/default.vue
@@ -68,6 +68,48 @@
 
     ) (document.querySelector ('#__nuxt'))
 
+
+    ;(header => {
+
+      if (!header) { return }
+
+      const fontSize = (fontSize =>
+
+        [ TWICE_54_BY_PHI_POWER_5, fontSize, TWICE_54_BY_PHI_POWER_3 ] [
+          +(fontSize > TWICE_54_BY_PHI_POWER_5)
+            + +(fontSize > TWICE_54_BY_PHI_POWER_3)
+        ]
+
+      ) (header.getBoundingClientRect ().height / PHI ** 2)
+
+
+      header.querySelectorAll ('nav').forEach (nav =>
+        nav.style.fontSize = `${ fontSize }px`
+      )
+
+    }) (document.querySelector ('#__nuxt > header'))
+
+
+    ;(footer => {
+
+      if (!footer) { return }
+
+      const fontSize = (fontSize =>
+
+        [ TWICE_54_BY_PHI_POWER_4, fontSize, TWICE_54_BY_PHI_POWER_2 ] [
+          +(fontSize > TWICE_54_BY_PHI_POWER_4)
+            + +(fontSize > TWICE_54_BY_PHI_POWER_2)
+        ]
+
+      ) (footer.getBoundingClientRect ().height / PHI ** 2)
+
+
+      footer.querySelectorAll ('*').forEach (nav =>
+        nav.style.fontSize = `${ fontSize }px`
+      )
+
+    }) (document.querySelector ('#__nuxt > footer'))
+
   } 
 
 
@@ -75,6 +117,9 @@
     setDimensions ()
     window.addEventListener ('resize', setDimensions)
   })
+
+  
+  onUpdated (setDimensions)
 
 </script>
 
