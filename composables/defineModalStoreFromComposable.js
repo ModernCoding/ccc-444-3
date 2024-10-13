2f4d06a7f4411762f4ae5c9f0baacb958ac71ddf
ccc.444 <contact@coding-crafting-consulting.com>
2024-10-13 16:45:47 +0700

    modalStore

--- composables/defineModalStoreFromComposable.js
+++ composables/defineModalStoreFromComposable.js
@@ -26,17 +26,47 @@ export default () => defineStore ('modal', {
 
   actions: {
 
+    patchAsideShow (content) {
+        this.modal.aside.content = content
+      },
+
     patchAsideShow (is = true) {
-      this.modal.aside.show = is
-    },
+        this.modal.aside.show = is
+      },
+
+
+    patchCentralShow (content) {
+        this.modal.central.content = content
+      },
 
     patchCentralShow (is = true) {
-      this.modal.central.show = is
-    },
+        this.modal.central.show = is
+      },
+
+
+    patchMainShow (content) {
+        this.modal.main.content = content
+      },
 
     patchMainShow (is = true) {
-      this.modal.main.show = is
-    }
+        this.modal.main.show = is
+      },
+
+
+    resetAside () {
+        this.modal.aside.content = null
+        this.modal.aside.show = false
+      },
+
+    resetCentral () {
+        this.modal.central.content = null
+        this.modal.central.show = false
+      },
+
+    resetMain () {
+        this.modal.main.content = null
+        this.modal.main.show = false
+      }
 
   }
 
