4f6a5ca8caf9ee020d1b6a8b9f90cbe901152387
ccc.444 <contact@coding-crafting-consulting.com>
2024-11-19 15:27:51 +0700

    minor

--- pages/contact/.partials/google-maps-link.vue
+++ pages/contact/.partials/google-maps-link.vue
@@ -5,7 +5,8 @@
     <a href="https://maps.app.goo.gl/pxzkFhtF86Hmse588" target="_blank">
 
       <div>
-        <i class="bi bi-geo-alt-fill" aria-hidden="true"></i>
+        <i class="bi bi-hand-index-fill" aria-hidden="true"></i>
+        <!-- <i class="bi bi-geo-alt-fill" aria-hidden="true"></i> -->
       </div>
 
       <strong class="o-font-en">Google&nbsp;Maps</strong>
