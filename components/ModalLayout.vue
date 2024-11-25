2cdd30abac74663acd5f0eeda481007fc49d8564
ccc.444 <contact@coding-crafting-consulting.com>
2024-11-25 14:45:45 +0700

    modalLayout

--- components/ModalLayout.vue
+++ components/ModalLayout.vue
@@ -6,8 +6,50 @@
   const { screenProperties } = storeToRefs (screenPropertiesStore)
 
 
-  onMounted (() => {
+  const _resize = () => (modalLayout => {
 
+      if (!modalLayout) { return }
+      modalLayout.removeAttribute ('style')
+
+      modalLayout.style.paddingTop
+        = `${ screenProperties.value.paddingTops.corrected.aside }px`
+
+
+      ;(modalContent => {
+
+        if (!modalContent) { return }
+        modalContent.removeAttribute ('style')
+
+
+        if (modalContent.scrollHeight <= modalContent.clientHeight) {
+          return modalContent.style.overflowY = 'hidden'
+        }
+
+
+        const modalPaddingLeft = parseInt (
+
+            window
+              .getComputedStyle (document.querySelector ('#__nuxt'))
+              .getPropertyValue ('padding-right')
+              .split ('px')
+              .join ('')
+
+          )
+
+
+        modalContent.style.paddingRight = `${ modalPaddingLeft / PHI }px`
+
+      }) (modalLayout.querySelector ('*'))
+
+    }) (document.querySelector ('#modal-layout'))
+
+
+  watch (screenPropertiesStore, _resize)
+
+
+  onMounted (() => {
+    _resize ()
+    window.addEventListener ('resize', _resize)
   })
 
 </script>
