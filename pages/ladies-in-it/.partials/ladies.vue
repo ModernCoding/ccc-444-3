66bca6a8f027eecd47c58e9e90a3c24b980a3e39
ccc.444 <contact@coding-crafting-consulting.com>
2024-11-25 22:15:54 +0700

    defineLadiesStoreFromComposable

---
+++ pages/ladies-in-it/.partials/ladies.vue
@@ -0,0 +1,107 @@
+<script setup>
+  
+  const ladiesStore = defineLadiesStoreFromComposable () ()
+
+  const screenPropertiesStore
+    = defineScreenPropertiesStoreFromComposable () ()
+
+
+  const { ladies } = storeToRefs (ladiesStore)
+  const { screenProperties } = storeToRefs (screenPropertiesStore)
+
+</script>
+
+
+<template>
+
+  <article id="ladies-in-it" class="d-flex flex-column">
+
+    <section class="d-flex align-items-center">
+
+      <figure>
+
+        <picture>
+        
+          <NuxtImg
+            src="/images/ada.png"
+            alt="Ada Lovelace"
+            loading="lazy"
+            class="w-100"
+          />
+        
+        </picture>
+        
+
+        <figcaption>Ada Lovelace (1815 - 1852)</figcaption>
+
+      </figure>
+
+
+      <h1>The first computer programmer in history was a woman</h1>
+    
+    </section>
+
+
+    <section class="d-flex align-items-center">
+
+      <figure>
+
+        <picture>
+        
+          <NuxtImg
+            src="/images/grace_no_transperency.png"
+            alt="Grace Hopper"
+            loading="lazy"
+            class="w-100"
+          />
+        
+        </picture>
+
+        
+        <figcaption>Grace Hopper (1906 - 1992)</figcaption>
+
+      </figure>
+        
+
+      <h1>
+        The first human-like programming language was designed by a woman
+      </h1>
+
+    </section>
+
+
+    <section class="d-flex align-items-center">
+
+      <figure>
+
+        <picture>
+        
+          <NuxtImg
+            src="/images/margaret.png"
+            alt="Margaret Hamilton"
+            loading="lazy"
+            class="w-100"
+          />
+        
+        </picture>
+
+        
+        <figcaption>Margaret Hamilton (1936 - )</figcaption>
+
+      </figure>
+      
+
+      <h1>NASA's first software engineer is a woman</h1>
+
+    </section>
+
+  </article>
+
+</template>
+
+
+<style
+  scoped
+  src="@/assets/sass/pages/ladies-in-it/index.sass"
+  lang="sass"
+></style>
