a48c560c57f08b74b5d42de29584b82d3cca1891
ccc.444 <contact@coding-crafting-consulting.com>
2024-11-26 15:57:00 +0700

    text

--- _locales/de/pages/ladiesInIt/_index.js
+++ _locales/de/pages/ladiesInIt/_index.js
@@ -6,6 +6,6 @@ export const index = {
   
     grace: "Oder: Die Mona Lisa der Informatik! %Sie% ist %die% Erfinder%in% der allerersten menschlich ähnlichen Programmiersprache und somit auch des allerersten Compilers.",
   
-    margaret: "Sie kam als allererste Person ins Apollo-Projekt der NASA und ist somit die allererste Software-Ingenieur in der Geschichte der Informatik."
+    margaret: "Als allererste Person kam %sie% ins Apollo-Projekt der NASA; somit bekam zum allerersten Mal in der Geschichte der Informatik den Titel Software-Ingenieur auch %sie%."
   
   }
