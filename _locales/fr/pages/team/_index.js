4282975ca739a1a79f8677e11a3612b78cc42a28
ccc.444 <contact@coding-crafting-consulting.com>
2024-11-29 13:59:49 +0700

    members

--- _locales/fr/pages/team/_index.js
+++ _locales/fr/pages/team/_index.js
@@ -1,4 +1,11 @@
 export const index = {
     title: "L'équipe",
-    all: "Nous sommes tous %fullstack% et même %davantage%\xa0!"
+    all: "Nous sommes tous %fullstack% et même %davantage%\xa0!",
+    fullstack: "Fullstack",
+    founder: "Entrepreneur",
+    consulting: "Conseil",
+    teamLeading: "Pilotage d'équipes externes",
+    graphicDesign: "Design Graphique",
+    animation: "Animation",
+    api: "Spécialiste A.P.I."
   }
--- _locales/my/pages/team/_index.js
+++ _locales/my/pages/team/_index.js
@@ -1,4 +1,11 @@
 export const index = {
     title: "The team",
     all: "We %all% do fullstack and even %more%!",
+    fullstack: "Fullstack",
+    founder: "Business Leader",
+    consulting: "Consulting",
+    teamLeading: "External Team-Leading",
+    graphicDesign: "Grafic Design",
+    animation: "Animation",
+    api: "API Specialist"
   }
