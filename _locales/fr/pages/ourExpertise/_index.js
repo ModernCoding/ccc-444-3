88147b5ec6f293c65dae2d4aff7ad5705970874e
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-01 16:20:03 +0700

    lang

---
+++ _locales/fr/pages/ourExpertise/_index.js
@@ -0,0 +1,4 @@
+export const index = {
+    slogan: "Nous unissons le Beau et la Haute Technicité.",
+    and: "Vous le visualisez à l'instant\xa0!"
+  }
---
+++ _locales/kh/pages/ourExpertise/_index.js
@@ -0,0 +1,4 @@
+export const index = {
+    slogan: "We unite Beauty and High Technicity.",
+    and: "And this is exactly waht you are seeing now!"
+  }
