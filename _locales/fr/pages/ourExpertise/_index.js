47d3ff6554038540fb23878dd2816dad71594da0
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-01 21:58:24 +0700

    fr

--- _locales/fr/pages/ourExpertise/_index.js
+++ _locales/fr/pages/ourExpertise/_index.js
@@ -2,6 +2,29 @@ export const index = {
 
     slogan: "Nous unissons le Beau et la Haute Technicité, comme le montre ce site web\xa0!",
 
-    title: "Notre Savoir-Faire"
+    title: "Notre Savoir-Faire",
+
+
+    expertise: {
+
+        craft: {
+            slogan: "Vos\xa0applis sur mesure, design graphique inclus\xa0!",
+            know: "Savoir",
+            how: "comment"
+          },
+
+        consulting: {
+            slogan: "Nos conseils pour vos propres développements\xa0!",
+            know: "En savoir",
+            how: "plus"
+          },
+
+        guestLead: {
+            slogan: "Nous dirigeons vos équipes en tant que chefs invités\xa0!",
+            know: "Savoir",
+            how: "pourquoi"
+          },
+      
+      }
 
   }
--- _locales/kh/pages/ourExpertise/_index.js
+++ _locales/kh/pages/ourExpertise/_index.js
@@ -1,7 +1,31 @@
 export const index = {
 
-    slogan: "We unite Beauty and High Technicity, as this website shows!",
+    slogan:
+      "We unite Beauty and High Technicity, as this website shows!",
 
-    title: "Our Expertise"
+    title: "Our Expertise",
+
+
+    expertise: {
+
+        craft: {
+            slogan: "We craft your\xa0apps, including grafic design!",
+            know: "Know",
+            how: "how"
+          },
+
+        consulting: {
+            slogan: "We help you craft yours!",
+            know: "Know",
+            how: "more"
+          },
+
+        guestLead: {
+            slogan: "We lead your teams as guest tech leads!",
+            know: "Know",
+            how: "why"
+          },
+      
+      }
 
   }
