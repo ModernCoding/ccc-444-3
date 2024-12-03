81472b05d258c4f4cc1cd65466ffcc5362a3b5f8
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-03 21:31:04 +0700

    fah

--- _locales/kh/pages/team/_member.js
+++ _locales/kh/pages/team/_member.js
@@ -65,6 +65,66 @@ export const member = {
       
         ]
 
+    },
+
+
+    mon: {
+
+      quote: [
+          
+          "ONESELF, INDEED, IS ONE’S SAVIOUR, FOR WHAT OTHER SAVIOUR WOULD THERE BE?",
+
+          "WITH ONESELF WELL CONTROLLED ONE OBTAINS A SAVIOUR DIFFICULT TO FIND."
+        
+        ],
+
+
+      by: `Dhammapada Chapter 12 “The Self (Attavaggo)”, verse 160`,
+
+
+      text: [
+
+          "Coming from a country going through an extreme political situation, I decided to %take action%, rather than awaiting foreign aid to save us!",
+
+          "I have been helping myself become an %experienced full stack developer% building %robust% and %high-performing% applications with great user experience using .NET, Java, JavaScript, Vue, React and RDBMS.",
+
+          "I have also put %much effort% to turn my %passion% in design and animation %into a profession% designing %custom icons% for websites and crafting %engaging animations% enhancing %visual storytelling%.",
+
+          "With this background combining technical expertise with artistic insight, I deliver solutions where %functionality and aesthetics are in harmony%.",
+
+          "With this ability to %bridge the gap% between technology and art, I packed my bag and fought against all odds to finally cross the bridge between Burma and Thailand!"
+      
+        ]
+
+    },
+
+
+    fah: {
+
+      quote: "Design is not just what it looks like and feels like. Design is how it works.",
+
+      by: `Steve Jobs`,
+
+      introduction: "Creating a website is like building a house: the more attention you put into the details, the more inviting and vibrant it becomes.",
+
+      text: [
+
+          `I come from the Trat province, Thailand, a place famous for its %"Bridge To Test Your Willingness"%.`,
+
+          "This must be why I have always had the %willingness to bridge technology and creativity%: since childhood, I have always loved inventing new things, as well as I have always been attracted by modern gadgets.",
+
+          "This passion led me to pursue a degree in Computer Science to explore how technology works.",
+
+          "After graduating, I also got the %willingness to bridge technology and art% as well!",
+
+          "My %willingness to bridge my technical background and my passion for graphic design% has led me to design websites that meet the users’ needs.",
+
+          "While art and technology seem to be two separate worlds, I make them blend together in order to create something %truly unique!%",
+
+          "To me, building a website is like building a house: it requires thoughtful planning, a well-thought design, and %attention to detail% to make it a place everyone wants to live in."
+      
+        ]
+
     }
 
   }
