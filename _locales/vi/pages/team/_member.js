e81fe6dc2da998cc0053b2baa6d166d0d1f8fecd
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-03 12:10:48 +0700

    babe

--- _locales/vi/pages/team/_member.js
+++ _locales/vi/pages/team/_member.js
@@ -5,38 +5,67 @@ export const member = {
     myanmar: "Myanmar",
     thailand: "Thailand",
 
-    text: [
+
+    michel: {
+
+      text: [
+          
+          `This sweet dream, that Friedrich Schiller expressed in his famous ode "An Die Freude", and which Ludwig van Beethoven later set to music in his legendary Ninth Symphony, well: most people are going farther away from it!`,
+
+          "On the opposite, %I am getting closer!%",
+
+          "In my still ongoing career, for sure I have worked with different programming languages and technologies for different domains; among many: COBOL, C#, Ruby, PHP, DB2, MySQL and now JavaScript with PostgreSQL for the banking and financial industry, welfare, e-commerce, startups, fintech.",
+
+
+          "%Most important is however:%",
+
+          `
+            > I have been living and working in several countries
+            > I have been working with people coming from completely different cultural backgrounds, be they colleagues or coworkers
+            > I have learned several languages and become fluent in 5 of them
+          `,
+
+          "This is how I have worked:",
+
+          `
+            > %in% France, Germany, Portugal, Spain, Vietnam, Cambodia and now Thailand
+            > %with% French, Germans, Italians, Portuguese, Spaniards, Vietnamese, Americans, Czechs, Khmers, Thais und Burmese
+          `
         
-        `This sweet dream, that Friedrich Schiller expressed in his famous ode "An Die Freude", and which Ludwig van Beethoven later set to music in his legendary Ninth Symphony, well: most people are going farther away from it!`,
-
-        "On the opposite, %I am getting closer!%",
-
-        "In my still ongoing career, for sure I have worked with different programming languages and technologies for different domains; among many: COBOL, C#, Ruby, PHP, DB2, MySQL and now JavaScript with PostgreSQL for the banking and financial industry, welfare, e-commerce, startups, fintech.",
+        ],
 
 
-        "%Most important is however:%",
+      cambodia: "In Cambodia, I have also...",
 
-        `
-          > I have been living and working in several countries
-          > I have been working with people coming from completely different cultural backgrounds, be they colleagues or coworkers
-          > I have learned several languages and become fluent in 5 of them
-        `,
+      mentoring:
+        "...instructed & mentored my customers' junior developers & fresh graduates as a %guest tech team leader%.",
 
-        "This is how I have worked:",
+      vietnam: "In Vietnam, I have also taught at Universities."
 
-        `
-          > %in% France, Germany, Portugal, Spain, Vietnam, Cambodia and now Thailand
-          > %with% French, Germans, Italians, Portuguese, Spaniards, Vietnamese, Americans, Czechs, Khmers, Thais und Burmese
-        `
+    },
+
+
+    babe: {
+
+      quote: "In the moment when I truly %understand% my enemy, understand him well enough to defeat him, then in that very moment I also %love% him.",
+
+      by: `Orson Scott Card, "Ender's Game"`,
+
+
+      text: [
+
+          "Road %building% and building %building% had always been a mystery to me, so I had decided to study Civil Engineering at the Kasetsart University, Sriracha Campus, Thailand, in order to %understand% these.",
+
+          "During my studies, I got the will to also %understand web development%. As soon as I started understanding it, I also %started loving it!%",
+
+          "So I decided to embrace a career in web development, %building% projects of %high quality%.",
+
+          "Right now, I am specializing in JavaScript technologies: Node.js, React, Nuxt, Sequelize, as well as PostgreSQL for the database.",
+
+          "I also conduct %research and development% in order to understand what others do not understand yet."
       
-      ],
+        ]
 
-
-    cambodia: "In Cambodia, I have also...",
-
-    mentoring:
-      "...instructed & mentored my customers' junior developers & fresh graduates as a %guest tech team leader%.",
-
-    vietnam: "In Vietnam, I have also taught at Universities."
+    }
 
   }
