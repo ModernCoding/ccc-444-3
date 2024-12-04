cad5a23b8a27127daea03efc1e1bc76b5a49945a
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-04 12:05:47 +0700

    ReadMore

--- composables/_scripts/team/_local/handleMembers.js
+++ composables/_scripts/team/_local/handleMembers.js
@@ -45,4 +45,55 @@ export const handleMembers = screenProperties => {
       document.querySelector ('#team-girls ul')
     )
 
+
+  const fontSize = TWICE_54_BY_PHI_POWER_5 * PHI ** (
+      +screen.isAtLeastSmHalf + +screen.isAtLeastLgHalf
+    )
+
+
+  document
+    .querySelectorAll ('.o-team-read-more')
+
+    .forEach (readMore => {
+      
+        readMore.removeAttribute ('style')
+        readMore.style.gap = `${ fontSize / PHI ** 2 }px`
+
+
+        readMore.querySelectorAll ('p').forEach ((p, i) => {
+          
+          p.style.fontSize = `${ fontSize }px`
+          p.style.height = `${ fontSize * PHI }px`
+
+
+          ;(span => {
+            if (!span) { return }
+            span.style.height = `${ fontSize }px`
+            span.style.width = `${ fontSize * PHI }px`
+          }) (p.querySelector ('span'))
+
+
+          ;(i => {
+            if (!i) { return }
+            i.style.fontSize = `${ fontSize / PHI }px`
+          }) (p.querySelector ('i'))
+
+
+          ;(strong => {
+            if (!strong) { return }
+            strong.style.fontSize = `${ fontSize }px`
+          }) (p.querySelector ('strong'))
+
+
+          i > 0 && (() => {
+              p.style.gap = `${ fontSize / PHI ** 2 }px`
+              p.style.paddingRight = `${ fontSize }px`
+            }) ()
+
+        })
+      
+      })
+
+
+
 }
