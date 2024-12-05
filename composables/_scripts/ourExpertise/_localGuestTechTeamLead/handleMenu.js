eaa3bb15c71edfdded909281419d3e1d60e11782
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-05 17:26:43 +0700

    menu

--- composables/_scripts/ourExpertise/_localGuestTechTeamLead/handleMenu.js
+++ composables/_scripts/ourExpertise/_localGuestTechTeamLead/handleMenu.js
@@ -16,20 +16,22 @@ export const handleMenu = screenProperties => {
   ;(screen =>
 
     (factor => {
+
+      menu.style.gap = `${ TWICE_54_BY_PHI_POWER_4 * factor }px`
     
       listItems.forEach (lI =>
-          lI.style.gap = `${ TWICE_54_BY_PHI_POWER_3 * factor }px`
+          lI.style.gap = `${ TWICE_54_BY_PHI_POWER_4 * factor }px`
         )
 
       menuItems.forEach (mI =>
-          mI.style.fontSize = `${ TWICE_54_BY_PHI_POWER_4 * factor }px`
+          mI.style.fontSize = `${ TWICE_54_BY_PHI_POWER_5 * factor }px`
         )
 
 
       screenProperties.ratioIndex < 3 && (
 
           menu.style.paddingBottom = `${ 
-              TWICE_54_BY_PHI_POWER_3
+              TWICE_54_BY_PHI_POWER_4
                 * factor
                 * PHI ** (+(screenProperties.ratioIndex < 2))
             }px`
