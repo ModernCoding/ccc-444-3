9953e6260d951249373c1d98da4b243b118c3aef
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-06 12:55:33 +0700

    text

--- _locales/de/pages/ourExpertise/_guestTechTeamLead.js
+++ _locales/de/pages/ourExpertise/_guestTechTeamLead.js
@@ -31,6 +31,43 @@ export const guestTechTeamLead = {
         >Maestro Riccardo Muti, der an diesem 1. Jänner 2025 das Neujahrskonzert dirigieren wird!
       `
 
+    ],
+
+
+  cambodia: [
+
+      "Ähnlich wie die Wiener Philharmoniker ohne permanente Dirigenten musizieren, entscheiden sich manche IT-Firmen, %ohne permanente technische Senior-TeamleiterInnen, Ihre Geschäfte zu führen!%",
+
+      "Lieber lassen sie %ihre Teams durch technische Gast-TeamleiterInnen% führen.",
+
+      "Dies ist der Fall von einem unserer Kunden: Die Firma BiKay in Phnom Penh, Kambodscha.",
+
+      "Diese Firma wurde von einem französischen Softwareingenieur gegründet, der über einen starken Hintergrund und umfangreiche Erfahrung im Programmieren verfügt.",
+
+      "Sie bietet Projektentwicklungsdienstleistungen für ihre internationale Kundschaft zu wettbewerbsfähigen Preisen an.",
+
+      "Durch meine Erfahrung:",
+
+      `
+        >als Gastprofessor an vietnamesischen und kambodschanischen Universitäten,
+        >als technischer Teamleiter in vergangen Firmen,
+        >als Firmengründer selber, der mein eigenes Team führt,
+      `,
+
+      "%zahle ich seit 2021 zu den technischen Gast-TeamleiterInnen meines Kunden für seine Juniorentwickler und frische Absolventen!%",
+
+      "Meine Verantwortungen sind immer folgende:",
+
+      `
+        >Ansprechpartner für die eigenen Kunden meines Kunden sein!,
+        >die Kundenbedürfnisse analysieren,
+        >Zeitaufwandschätzungen liefern,
+        >den passenden Tech-Stack wählen,
+        >die Verantwortung für die Qualität des Projekts tragen,
+        >das technische Know-How an die Kundenjunioren übertragen,
+        >denen die guten Praxis und das Selbst-Lernen beibringen
+      `,
+
     ]
 
 }
