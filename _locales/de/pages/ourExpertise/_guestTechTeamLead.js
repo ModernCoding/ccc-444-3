a5aa52b13a30c68262c867d573573afa7383b22b
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-06 14:00:49 +0700

    text

--- _locales/de/pages/ourExpertise/_guestTechTeamLead.js
+++ _locales/de/pages/ourExpertise/_guestTechTeamLead.js
@@ -36,9 +36,9 @@ export const guestTechTeamLead = {
 
   cambodia: [
 
-      "Ähnlich wie die Wiener Philharmoniker ohne permanente Dirigenten musizieren, entscheiden sich manche IT-Firmen, %ohne permanente technische Senior-TeamleiterInnen, Ihre Geschäfte zu führen!%",
+      "Ähnlich wie die Wiener Philharmoniker ohne permanente Dirigenten musizieren, entscheiden sich manche IT-Firmen, %ohne permanente technische Senior-Teamleiter*innen, Ihre Geschäfte zu führen!%",
 
-      "Lieber lassen sie %ihre Teams durch technische Gast-TeamleiterInnen% führen.",
+      "Lieber lassen sie %ihre Teams durch technische Gast-Teamleiter*innen% führen.",
 
       "Dies ist der Fall von einem unserer Kunden: Die Firma BiKay in Phnom Penh, Kambodscha.",
 
@@ -56,7 +56,7 @@ export const guestTechTeamLead = {
       `,
 
       "",
-      "%zähle ich seit 2021 zu den technischen Gast-TeamleiterInnen meines Kunden für seine Juniorentwickler und frische Absolventen!%",
+      "%zähle ich seit 2021 zu den technischen Gast-Teamleiter*innen meines Kunden für seine Juniorentwickler und frische Absolventen!%",
 
       "",
       "Dabei sind meine Verantwortungen immer folgende:",
@@ -75,6 +75,34 @@ export const guestTechTeamLead = {
 
 
   benefits: [
+
+      "Als selbstverständlichsten Vorteil, Ihre Teams durch Gast-Teamleiter*innen führen zu lassen, nennen wir die %Kosteneinsparungen!%",
+
+      `
+        >Sie müssen nicht, permanente Senior-Teamleiter*innen zu bezahlen, auch wenn Sie sich momentan ohne Kundenprojekte befinden...
+        >Permanente Senior-Teamleiter*innen kommen mit Fixkosten und den dazugehörigen Steuern, die Sie auch noch bezahlen sollen! Gast-Teamleiter*innen können Sie ganz im Gegenteil zu den Firmenausgaben einfügen und von Ihren Betriebssteuern absetzen!
+      `,
+
+
+      "",
+      "Ein weiterer Vorteil liegt im %verstärkten Vertrauen%, auf dem jedes Geschäft ruht!",
+
+      `
+        >In einer Branche wie Informatik, ist es höchst wichtig, anzuerkennen, %wenn man etwas nicht weiß oder beherrscht%. Den Team-Mitgliedern ist es deutlich einfacher, es zu gestehen, wenn das Projekt von einem Gast-Teamleiter geführt wird, als wenn ein interner Manager es tut.
+        >Hassen Ihre Teams ihren Manager, verlassen sie meistens Ihre Firma nach dem Motto "Mitarbeiter verlassen sie nicht schlechte Firmen, sondern schlechte Managers"! Als Betriebsleiter müssen Sie dann neue Mitarbeiter anstellen und ausbilden, was zu erhöhten Kosten führt. Oder sie müssen in der Not dringend erfahrene Senioren finden, die sich dann teuer verkaufen!
+        >Wenn Ihre Teams im Gegenteil den Gast-Teamleiter nicht mögen, haben Sie die Freiheit, ihn nicht mehr zu rufen.
+      `,
+
+
+      "",
+      "Weitere Gründe, warum Sie Gast-Teamleiter*innen in Betracht ziehen könnten:",
+
+      `
+        >Anders als bei manchen permanenten Teamleitern, die sich auf Lorbeeren setzen und mit dem Haben-wir-immer-so-gemacht-Spruch alle Innovationen bremsen, sind Gast-Teamleiter*innen gezwungen, ständig mit den neuesten Technologien vertraut zu sein, und %immer innovativ zu bleiben%, wenn sie von Kunden immer wieder gerufen wollen! 
+        >Während wir auf JavaScript-Technologien spezialisiert sind, sind Ihre Teams mit ganz anderen Tech-Stacks vertraut, und sie müssen schnell umlernen. 
+        >Ihre Teams sind sogar allergisch gegen JavaScript geworden, und eine Versöhnung mit dieser schwer vermeidbaren Technologie ist nötig! 
+      `,
+
     ]
 
 }
