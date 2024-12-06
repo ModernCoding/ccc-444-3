e79154159faafde3904ae1ae1fc81e58e4ac3d9c
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-06 12:03:09 +0700

    text

--- _locales/de/pages/ourExpertise/_guestTechTeamLead.js
+++ _locales/de/pages/ourExpertise/_guestTechTeamLead.js
@@ -18,7 +18,18 @@ export const guestTechTeamLead = {
 
       "Zu den prestigeträchtigsten Musikensembles der Welt zählen die Wiener Philharmoniker.",
 
-      "Dieses Orchester hat eine Besonderheit, die andere Orchester gleicher Liga nicht haben: %Die Musiker spielen seit 1933 ohne permanente Dirigenten!%"
+      "Dieses Orchester hat eine Besonderheit, die andere Orchester gleicher Liga nicht haben: %Die Musiker spielen% seit 1933 %ohne permanente Dirigenten!%",
+
+      "Tatsächlich spielen sie jedes Konzert %ausschließlich mit Gast-Dirigenten%, die selber Chef-Dirigenten anderer prestigiösen Orchester sind.",
+
+      "Man denke beispielsweise, um nur einige unter vielen anderen zu nennen, an:",
+
+      `
+        >den hochberühmten ehemaligen Dirigenten der Berliner Philharmoniker Herbert von Karajan
+        >seinen legendären Vorgänger Wilhelm Furtwängler
+        >Herrn Kapellmeister Christian Thielemann, der das Neujahrskonzert der Wiener Philharmoniker 2024 dirigierte
+        >Maestro Riccardo Muti, der an diesem 1. Jänner 2025 das Neujahrskonzert dirigieren wird!
+      `
 
     ]
 
