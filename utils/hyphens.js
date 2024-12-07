a0aeae4fbee6ca2b5d18ac1ce5400a4db5ec3f41
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-07 23:00:36 +0700

    hyphen

--- utils/hyphens.js
+++ utils/hyphens.js
@@ -2,7 +2,6 @@ import * as moduleDe from 'hyphen/de'
 import * as moduleEn from 'hyphen/en'
 import * as moduleFr from 'hyphen/fr'
 
-
 export const hyphenate = (sentence, locale) =>
 
   ![ 'de', 'en', 'fr' ].includes (locale) ? sentence : {
