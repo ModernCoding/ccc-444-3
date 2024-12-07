aca0ae5d2ac954bb84d0a68f0637eff07e78f55c
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-07 13:13:32 +0700

    text

---
+++ _locales/fr/pages/ourExpertise/_consulting.js
@@ -0,0 +1,76 @@
+export const consulting = {
+  
+  title: "We are happy to help and give advice!",
+
+
+  menu: [
+
+      "mvp-with-laravel|In case you want to build your mvp yourself",
+
+      "hotwire-on-laravel|In case you already have an app, that has not been developed with JavaScript"
+  
+    ],
+
+
+  mvp: [
+
+      "While we focus on JavaScript technologies, we remain open to alternative tech stacks: the ones our customers prefer to use.",
+
+      "In case you would like to develop your own project yourself, and in case you favor %a framework not based on JavaScript%, such as Ruby on Rails, Django or Laravel, we are happy to help!",
+
+      "We have done it with Huneety.",
+
+      "~https://www.huneety.com",
+
+      "Huneety is a HR-company founded by two French in Bangkok, Thailand.",
+
+      "Huneety is a the same time a web portal, specifically designed for the needs of the Thai job market, that are not covered by LinkedIn.",
+
+      "The web portal MVP has been developed by one of the co-founders, %although he originally has no backgrounf in IT!% His background is sales and marketing.",
+
+      "He started learning coding with PHP; for his MVP, he took the logical decision to choose the Laravel framework and began with the development.",
+
+      "Later, when he got the feeling to have reached a technical dead-end road, he found our company and asked for our help, since I already had experience with Laravel as I was living in Cambodia.",
+
+      "This is how I helped him:",
+
+      `
+        >I first advised him to start over! It was easy to spot, that he had started from an obsolete boilerplate with outdated libraries
+        >together with him, we took the time to properly design the database, so that it would cover all business needs %on the long term and in a highly felexible way%
+      `,
+
+
+      "I helped him as a 100% consultant, in which we met weekly for a 2-hour work session.",
+
+      `
+        >We discussed his needs.
+        >I gave him advice, not only on technical aspects: user experience, ergonomics, layout, etc...
+        >Like a teacher giving homeworks to his student, I listed all tasks he would need to complete until the next work session: migration files, corresponding models, views and controllers.
+        >At the beginning on the following session, we fixed together what was needed to be fixed.
+      `
+
+    ],
+
+
+  hotwire: [
+
+      "If you already have a web app, that has not been developed with JavaScript frameworks, but with another full stack technology such as Ruby on Rails, Django or Laravel, you might have noticed, %how slow it can be sometimes!%",
+
+      "This is due to the intrinsic nature of the - voluminous - data exchange between HTTP requests and responses.",
+
+      "%The solution exists: Hotwire.%",
+      "~https://www.hotwired.dev",
+
+      "It was designed by David Heinemeier Hansson, the creator of Ruby on Rails, as a minimalistic JavaScript framework.",
+
+      "This framework is well suited for teams, who wish to keep the usage of JavaScript at a strict minimum!",
+
+      "Even if it is recommended on the long term to separate the front from the backend, Hotwire can be a good interim solution to be integrated into your existing project in order to increase the performances of your web app!",
+
+      "This is exactly the solution I implemented into Cambodian e-commerce portal Kokopon:",
+
+      "~https://www.kokopon.com"
+  
+    ]
+
+}
--- _locales/my/pages/ourExpertise/index.js
+++ _locales/my/pages/ourExpertise/index.js
@@ -1,8 +1,10 @@
+import { consulting } from "./_consulting"
 import { guestTechTeamLead } from "./_guestTechTeamLead"
 import { index } from "./_index"
 
 
 export const ourExpertise = {
+    consulting,
     guestTechTeamLead,
     index
   }
