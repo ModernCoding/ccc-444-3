31df78779b380574d7839002bfda3b2c6847fa40
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-08 18:40:53 +0700

    en

---
+++ _locales/en/pages/ourExpertise/_ourApproach.js
@@ -0,0 +1,123 @@
+export const ourApproach = {
+  
+  title: "Our app development approach",
+
+
+  menu: [
+
+      "on-the-same-boat|Designers & Developers on the same boat!",
+
+      "innovative-layout|We have developed a new & innovative website layout!",
+
+      "focus-on-javascript|We are JavaScript specialists & we focus on it!"
+  
+    ],
+
+
+  onTheSameBoat: [
+
+      "Wenn Sie sich als Kunde eine Web- oder Mobile-App wünschen, gehen Sie meistens nach Plan A vor.",
+
+      `
+        >Sie oder die von Ihnen beauftragten Entwickler fragen Grafikdesigner nach dem Design Ihrer App.
+        >Die Grafikdesigner entwerfen ein ansprechendes Design, wobei sie mit Farben, Schriftarten, Verläufen, Linienbreiten und insbesondere mit Transparenz und Opazität spielen.
+        >Nach zahlreichen Mag-ich-mag-ich-nicht-Gesprächen und möglicherweise einer von Ihnen bereits gezahlten Summe einigen Sie sich schließlich auf das entgültige Design, das entwickelt werden soll!
+        >Die Entwicklungsteams erhalten das Design, auf dessen Grundlage sie Kosteneinschätzungen vornehmen. Nach Ihrer Zustimmung beginnen sie mit der Entwicklungsarbeit.
+      `,
+
+      "",
+      "Die Entwickler stellen dann fest, dass sich das Design doch nicht genau so umsetzen lässt, wie es die Grafikdesigner ursprünglich entworfen haben!",
+
+      `
+        >Die leicht durchsichtigen Bilder erzeugen aufgrund des farbigen Hintergrunds nicht den gewünschten Effekt und können auch nicht mittels CSS korrigiert werden.
+        >Die Designer haben bestimmte Bildschirmgrößen übersehen, sodass einige Designelemente einfach nicht hineinpassen!
+        >Die Designer haben das Design mit englischen Texten erstellt. Doch die Titel der deutschen Version Ihrer mehrsprachigen App benötigen deutlich mehr Platz, der im entworfenen Design nicht vorgesehen ist!
+        >Usw...
+      `,
+
+
+      "",
+      "Ab diesem Punkt haben Sie nur noch zwei Möglichkeiten.",
+
+      `
+        >Sie bitten die Designfirma um Änderungen, was zu Mehrkosten führt.
+        >Sie lassen die Entwickler Notlösungen implementieren: Farbige Vierecke hinter den Bildern einfügen, die möglicherweise das Farbschema stören, auf einige Designelemente verzichten, Textgrößen oder -längen reduzieren, auch wenn dadurch die Bedeutung leidet...
+      `,
+
+
+      "",
+      "Im Endeffekt erhalten Sie ein Ergebnis, dass Sie sich nicht wünschten...",
+
+      "Andere Auftraggeber verfahren daher nach Plan B.",
+
+      `
+        >Sie beauftragen eine IT-Firma, eine App ohne vorheriges Design zu entwickeln.
+        >Nach dem vollendeten Entwicklungsprozess wird eine Designfirma beauftragt, die App zu bekleiden.
+        >Die IT-Firma wird dann die notwendigen Anpassungen vornehmen, was zu Mehrkosten führt.
+        >Vielleicht werden die Entwickler auf die gleichen Herausforderungen wie beim Plan A stoßen!
+      `,
+
+
+      "",
+      "Ob Plan A oder Plan B, das gleiche Problem tritt auf: %Designer sind keine Entwickler%, und %Entwickler sind auch keine Entwickler!%",
+
+      "Als Kunde können Sie weder von einem %Künstler% verlangen, dass er %die technischen Herausforderungen% vorhersieht, noch von einem %Techniker% erwarten, dass er %künstlerische Korrekturen% vornimmt!",
+
+      "",
+      "Deshalb kommen wir mit unserem Plan ¶CCC¶: %Designer u. Entwickler auf dem gleichen Schiff%, oder im gleichen Haus %zu haben!%",
+
+      `
+        >%Erstens sind unsere beiden Designer auch Entwickler%, weshalb sie sich als Künstler der technischen Herausforderungen bewusst sind, wenn sie ein Design entwerfen.
+        >Sobald sie ein Design entwerfen, stellen wir als Entwickler sofort sicher, dass es sich gut implementieren lässt!
+        >Bei notwendigen Designkorrekturen können wir %deutlich schneller und ohne Mehrkosten% reagieren.
+        >Als mehrsprachige Entwickler stellen wir auch sicher, dass die verschiedenen Texte in den verschiedenen Sprachen in ihre Elemente passen!
+        >%Als JavaScript-Spezialisten sorgen wir auch dafür!%
+      `,
+
+    ],
+
+
+  innovativeLayout: [
+
+      "Die Geschichte der Web-Entwicklung begann, als die meisten Bildschirme ein Verhältnis von 4:3 bzw. eine Größe von höchstens 15 Zoll hatten.",
+
+      `Nach dem Aufkommen des Smartphones konzentrierte man sich entweder auf die Mobile-Entwicklung oder auf die Web-Entwicklung mit dem 'Mobile-First-Approach'.`,
+      
+      "Es ging vor allem darum, die Webseiten möglichst für die kleinsten Geräte anzuzeigen!",
+
+      "Mittlerweile sind Bildschirme verfügbar, die mit einem Verhältnis von %32:9% kommen, und deren Größe manchmal %57 Zoll% erreichen!",
+
+      "Doch niemand schien sich Gedanken darüber gemacht zu haben, wie man Webseiteninhalte auch für diese riesigen Geräte anzeigen kann: Bereits für 24-Zoll- bzw. 16:10-Bildschirme begrenzen die meisten Web-Entwickler die Breite auf etwa 1200 Pixel.",
+
+      "Wenn Sie eine solche Webseite auf einem 7680-Pixel-breiten Bildschirm besuchen, sehen Sie eine etwa 1200-Pixel breite Säule in der Mitte; der Rest bleibt völlig leer.",
+
+      "%Deshalb haben wir ein neues und innovatives Layout entwickelt%, mit dem Webseiten sowohl auf winzigen Flip-Smartphones als auch auf riesigen 57-Zoll-Gaming-Monitoren sowie auf konventionellen 27-Zoll-Desktop-Bildschirmen angezeigt werden können!",
+
+      "%Genau dieses Layout verwenden wir für die Webseite, die Sie gerade besuchen!% Sie können selbst feststellen, wie sich das Design an jede Gerätegröße anpasst!",
+
+      "%Als JavaScript-Spezialisten haben wir auch dafür gesorgt!%"
+
+    ],
+
+
+  focusOnJavaScript: [
+
+      "Natürlich kann man eine Webseite auch mit einem Framework erstellen, das nicht auf JavaScript basiert: Das haben wir bei unseren früheren Arbeitgebern mit PHP, Ruby oder Java umgesetzt.",
+
+      "Dass wir uns jetzt auf eine spezifische Programmiersprache konzentrieren, erklären wir folgendermaßen: %Wir vermeiden unnötigen Zeit- und Energieaufwand% für verschiedene Technologien, mit denen letztlich %das Gleiche erstellt werden kann% – nämlich Fullstack-Web-Apps!",
+
+      "JavaScript hat den Vorteil, sowohl im Browser als auch auf dem Server ausgeführt werden zu können.",
+
+      "Mit JavaScript hat man außerdem den Vorteil, mehr als nur Fullstack-Web-Apps erstellen zu können: Es ermöglicht auch die Entwicklung von Mobile-Apps, APIs, Blockchain-Anwendungen, Machine-Learning-Anwendungen und fortschrittlichen 3D-Animationen!",
+
+      "Ein weiterer Vorteil besteht darin, dass %herkömmliches JavaScript mit gängigen Open-Source-Bibliotheken% verwendet werden kann!",
+
+      "Sie sind nicht gezwungen, spezielle Komponenten und Module %eigens anfertigen zu lassen% oder %dauerhaft an einen Anbieter gebunden zu bleiben%.",
+
+      "Aus diesem Grund verzichten wir bewusst auf die Nutzung von CMS und fertigen Lösungen, die von Drittanbietern abhängig sind und die Verfügbarkeit Ihrer App gefährden könnten.",
+
+      "%Als JavaScript-Spezialisten sorgen wir dafur%, dass die Wartung und Weiterentwicklung der von uns erstellten Apps auch unabhängig von uns möglich ist."
+
+    ]
+
+}
