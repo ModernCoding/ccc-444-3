5809a8cbe47310f91ce706d9c28a2e38568c624a
ccc.444 <contact@coding-crafting-consulting.com>
2024-12-08 19:37:09 +0700

    en

--- _locales/my/pages/ourExpertise/index.js
+++ _locales/my/pages/ourExpertise/index.js
@@ -1,10 +1,12 @@
 import { consulting } from "./_consulting"
 import { guestTechTeamLead } from "./_guestTechTeamLead"
 import { index } from "./_index"
+import { ourApproach } from "./_ourApproach"
 
 
 export const ourExpertise = {
     consulting,
     guestTechTeamLead,
-    index
+    index,
+    ourApproach
   }
